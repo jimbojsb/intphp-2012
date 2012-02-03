583db6b6196eda7d626bd384b2d5d60f1d5a3a07
Josh Butts <josh@joshbutts.com>
2012-02-02 18:20:42 -0600

    db.php

--- bookmarks/lib/Db.php
+++ bookmarks/lib/Db.php
@@ -6,17 +6,33 @@ class Db
      */
     protected static $conn;
 
+    /**
+     * @static
+     * @param $hostname
+     * @param $username
+     * @param $password
+     * @param $database
+     */
     public static function init($hostname, $username, $password, $database)
     {
         $p = new PDO("mysql:host=$hostname;dbname=$database", $username, $password);
         self::$conn = $p;
     }
 
-    public static function query($sql, $params = null)
+    /**
+     * @static
+     * @param $sql string of SQL to execute
+     * @param array $params array of params in the order of the ?'s with in the SQL statement
+     * @return array an array of arrays which reperesent the SQL result set
+     */
+    public static function query($sql, $params = array())
     {
-        $result = self::$conn->query($sql);
-        return $result;
+        $stmt = self::$conn->prepare($sql);
+        $stmt->execute($params);
+        $stmt->setFetchMode(PDO::FETCH_ASSOC);
+        while ($resultRow = $stmt->fetch()) {
+            $results[] = $resultRow;
+        }
+        return $results;
     }
 }
-
-Db::query("SELECT 1");
